f940bfc9f77ee5057eccdb2d4f1c1360a9db8025
Meeaad Bharoochi <meeaadbharoochi@gmail.com>
2024-11-02 20:50:27 +0200

    menu update

--- src/components/Taskbar.js
+++ src/components/Taskbar.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import '../style/Taskbar.css';
 import win98StartMenuImage from '../images/Win98StartMenu.webp';
 import Win98Volume from '../images/Win98Volume.png';
-import Tablemountainmenu from '../images/Tablemountainmenu.png';
+import sidebar95 from '../images/sidebar95.png';
 import Meeaadheadshot from '../images/Meeaadheadshot.jpg';
 import linkedinlogo from '../images/linkedinlogo.jpg';
 import Resumeicon from '../images/Resumeicon.png';
@@ -37,11 +37,11 @@ const toggleAboutMe = () => {
         <img src={win98StartMenuImage} alt="Start Menu Icon" /> Start
       </button>
       {startMenuOpen && (
-        <div className="menubanner">
-          <img src={Tablemountainmenu} alt="Table mountain menu" className="Tablemountainmenu-icon"/>
-          <div className="menuheadshot">
-            <img src={Meeaadheadshot} alt="Meeaadheadshot" className="Meeaadheadshot"/>
             <div className="menu">
+             <div className="menusidebar">
+              <img src={sidebar95} />
+              </div>
+             <div className="menulist">
               <ul>
                 <li><img src={linkedinlogo} alt="LinkedIn Logo"/><a href="https://www.linkedin.com/in/meeaadbharoochi/" target="_blank">LinkedIn</a></li>
                 <li onClick={toggleAboutMe}><img src={Aboutme} alt="About me"/>About Me</li>
@@ -49,9 +49,8 @@ const toggleAboutMe = () => {
                 <li><img src={Github} alt="Github icon"/><a href="https://github.com/Meeaad021" target="_blank">Github</a></li>
                 <li><a href="mailto:meeaadbharoochi@gmail.com"><img src={Gmail} alt="Gmail icon"/>Gmail</a></li>
               </ul>
+              </div>
             </div>
-          </div>
-        </div>
       )}
       {AboutMeOpen && (
   <AboutMeContent
